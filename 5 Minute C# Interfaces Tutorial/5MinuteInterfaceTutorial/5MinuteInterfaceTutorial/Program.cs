8cbbec13ff0ecc8926f96070973a678d034be7e4
minTwin <michaelmin2020@gmail.com>
2022-02-11 19:01:32 +0100

    Initial Commit

---
+++ 5 Minute C# Interfaces Tutorial/5MinuteInterfaceTutorial/5MinuteInterfaceTutorial/Program.cs
@@ -0,0 +1,67 @@
+﻿using System;
+
+namespace _5MinuteInterfaceTutorial
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            // interface = defines a "contract" that all the classes inheriting from should follow
+
+            //             An interface declares "what a class should have"
+            //             An inheriting class defines "how it should do it"
+
+            //             benefits = security + multiple inheritance + "plug-and-play"
+
+            Rabbit rabbit = new Rabbit();
+            Hawk hawk = new Hawk();
+            Fish fish = new Fish();
+
+            rabbit.Flee();
+            hawk.Hunt();
+            fish.Flee();
+            fish.Hunt();
+
+            Console.ReadKey();
+        }
+
+        private interface IPrey
+        {
+            void Flee();
+        }
+
+        private interface IPredator
+        {
+            void Hunt();
+        }
+
+        private class Rabbit : IPrey
+        {
+            public void Flee()
+            {
+                Console.WriteLine("The rabbit runs away!");
+            }
+        }
+
+        private class Hawk : IPredator
+        {
+            public void Hunt()
+            {
+                Console.WriteLine("The hawk is searching for food!");
+            }
+        }
+
+        private class Fish : IPrey, IPredator
+        {
+            public void Flee()
+            {
+                Console.WriteLine("The fish swims away!");
+            }
+
+            public void Hunt()
+            {
+                Console.WriteLine("The fish is searching for smaller fish!");
+            }
+        }
+    }
+}
